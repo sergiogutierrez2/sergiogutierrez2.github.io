b88f1fd0769fee8af9619a5ab1f6efe789e0d0d5
SergioGutierrez <sergio.gutierrez@sjsu.edu>
2022-09-29 03:21:56 -0800

    updating

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -24,3 +24,31 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+var slideIndexTwo = 1;
+showSlidesTwo(slideIndexTwo);
+
+function plusSlidesTwo(nTwo) {
+  showSlidesTwo(slideIndexTwo += nTwo);
+}
+
+function currentSlideTwo(nTwo) {
+  showSlidesTwo(slideIndexTwo = nTwo);
+}
+
+
+function showSlidesTwo(nTwo) {
+  var iTwo;
+  var slidesTwo = document.getElementsByClassName("mySlidestwo");
+  var dotsTwo = document.getElementsByClassName("dottwo");
+  if (nTwo > slidesTwo.length) {slideIndexTwo = 1}    
+  if (nTwo < 1) {slideIndexTwo = slidesTwo.length}
+  for (iTwo = 0; iTwo < slidesTwo.length; iTwo++) {
+      slidesTwo[iTwo].style.display = "none";  
+  }
+  for (iTwo = 0; iTwo < dotsTwo.length; iTwo++) {
+      dotsTwo[iTwo].className = dotsTwo[iTwo].className.replace(" active", "");
+  }
+  slidesTwo[slideIndexTwo-1].style.display = "block";  
+  dotsTwo[slideIndexTwo-1].className += " active";
+}
